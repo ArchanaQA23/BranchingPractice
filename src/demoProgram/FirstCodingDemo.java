1b476b3fc6a90b8225664bfa930700c0b62a358d
OraniumTech <oraniumtech@gmail.com>
2023-04-03 11:54:04 +0530

    First commit

---
+++ src/demoProgram/FirstCodingDemo.java
@@ -0,0 +1,25 @@
+package demoProgram;
+
+public class FirstCodingDemo {
+	
+	int x=10;//10-->x
+	
+	
+	public void printNumber()
+	{
+		
+		System.out.println(x);
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		FirstCodingDemo f = new FirstCodingDemo();
+		f.printNumber();
+	
+	
+	}
+	
+	
+
+}
